a192d9c8fb71179328d1c5bf952c601ed3618bd7
kien19quang <kien19quang@gmail.com>
2023-05-19 12:12:55 +0700

    config antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import EmptyLayout from '@/layouts/EmptyLayout/EmptyLayout';
 import { AppPropsWithLayout } from '@/models/common';
 import '@/styles/globals.css';
-import Theme from '@/styles/theme';
-
+import Theme from '../styles/theme'
+import '../../public/antd.min.css';
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
 
