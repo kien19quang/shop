bf514db31b409a4227fd94eb3ff321acdd0c8e31
kien19quang <kien19quang@gmail.com>
2024-09-18 14:26:48 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
 
 const nextConfig = {
   reactStrictMode: false,
-  transpilePackages: ['antd'],
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
   env: {
     BACKEND_URL: 'http://127.0.0.1:5000',
     NEXTAUTH_SECRET: "shop-access-token"
