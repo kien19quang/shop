9e4938c11b715058ad0b3a7396d50388a6814da4
kien19quang <kien19quang@gmail.com>
2024-09-18 15:28:27 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     BACKEND_URL: process.env.APP_MODE === 'DEV' ? 'http://127.0.0.1:5000' : 'https://shop.fabuleuse.co',
     NEXTAUTH_SECRET: 'shop-access-token',
-    NEXTAUTH_URL: process.env.APP_MODE === 'DEV' ? 'http://localhost:3000' : 'https://shop-genius.vercel.app'
+    NEXTAUTH_URL: process.env.APP_MODE === 'DEV' ? 'http://localhost:3000/' : 'https://shop-genius.vercel.app/'
   }
 }
 
